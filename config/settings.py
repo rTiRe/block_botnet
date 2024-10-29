73e2d0a69e075281293a86a77a7485818d87ae62
rTiRe <zientenin@mail.ru>
2024-10-29 15:46:17 +0300

    initial project

---
+++ config/settings.py
@@ -0,0 +1,39 @@
+from pydantic_settings import BaseSettings
+
+from typing import Optional
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    BOT_WEBHOOK_PATH: str
+
+    CRYPTO_PAY_TOKEN: str
+    CRYPTO_WEBHOOK_PATH: str
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    WEBHOOK_URL: Optional[str]
+
+    @property
+    def db_url(self) -> str:
+        protocol='postgresql+asyncpg'
+        user_data = f'{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}'
+        server_data = f'{self.POSTGRES_HOST}:{self.POSTGRES_PORT}'
+        return f'{protocol}://{user_data}@{server_data}/{self.POSTGRES_DB}'
+
+    @property
+    def bot_webhook_url(self) -> str:
+        return f'{self.WEBHOOK_URL}/{self.BOT_WEBHOOK_PATH}'
+
+    @property
+    def crypro_webhook_url(self) -> str:
+        return f'{self.WEBHOOK_URL}/{self.CRYPTO_WEBHOOK_PATH}'
+
+    class Config:
+        env_file = 'config/.env'
+
+
+settings = Settings()
