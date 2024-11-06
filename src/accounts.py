612142ea59d0ab10011e6301e940342c5aa28554
rTiRe <zientenin@mail.ru>
2024-11-07 00:49:07 +0300

    update timeout seconds

--- src/accounts.py
+++ src/accounts.py
@@ -16,7 +16,7 @@ async def _setup_account(session_name: str) -> None | Client:
         proxy=settings.proxy,
     )
     try:
-        async with asyncio.timeout(5):
+        async with asyncio.timeout(15):
             is_authorized = await app.connect()
     except asyncio.TimeoutError:
         print(f'{session_name} - timed out', flush=True)
