d861bb84363fd620c43bd2cb09b7ae4b555e5894
rTiRe <zientenin@mail.ru>
2024-11-06 17:38:12 +0300

    update checker

--- sessions/checker.py
+++ sessions/checker.py
@@ -7,33 +7,34 @@ API_ID = 'supersecret'
 API_HASH = 'supersecret'
 
 
-async def check() -> tuple[int, int]:
+async def check_client(session: str) -> None:
+    app = Client(session, api_id=API_ID, api_hash=API_HASH)
+    is_authorized = await app.connect()
+    if not is_authorized:
+        print(f'{session} failed')
+    try:
+        me = await app.get_me()
+    except (
+        errors.ActiveUserRequired,
+        errors.AuthKeyInvalid,
+        errors.AuthKeyPermEmpty,
+        errors.AuthKeyUnregistered,
+        errors.AuthKeyDuplicated,
+        errors.SessionExpired,
+        errors.SessionPasswordNeeded,
+        errors.SessionRevoked,
+        errors.UserDeactivated,
+        errors.UserDeactivatedBan,
+    ):
+        print(f'{session} failed')
+    else:
+        print(f'"{session}" logged in as', me.phone_number)
+
+
+async def checker() -> tuple[int, int]:
     files = listdir('sessions')
     print(files)
     sessions = [file_name[:-8] for file_name in files if file_name.endswith('.session')]
-    for session in sessions:
-        app = Client(session, api_id=API_ID, api_hash=API_HASH)
-        is_authorized = await app.connect()
-        if not is_authorized:
-            print(f'{session} failed')
-            continue
-        try:
-            me = await app.get_me()
-        except (
-            errors.ActiveUserRequired,
-            errors.AuthKeyInvalid,
-            errors.AuthKeyPermEmpty,
-            errors.AuthKeyUnregistered,
-            errors.AuthKeyDuplicated,
-            errors.SessionExpired,
-            errors.SessionPasswordNeeded,
-            errors.SessionRevoked,
-            errors.UserDeactivated,
-            errors.UserDeactivatedBan,
-        ):
-            print(f'{session} failed')
-            continue
-        else:
-            print(f'"{session}" logged in as', me.phone_number)
+    await asyncio.gather(*[check_client(session) for session in sessions])
 
-asyncio.run(check())
+asyncio.run(checker())
