ce0a168cf9983f5b1c8a13da9bb9f356315ecb17
rTiRe <zientenin@mail.ru>
2024-11-06 21:17:03 +0300

    Update checker

--- sessions/checker.py
+++ sessions/checker.py
@@ -6,12 +6,14 @@ from pyrogram import Client, errors
 API_ID = 'supersecret'
 API_HASH = 'supersecret'
 
+success_logged = []
+
 
 async def check_client(session: str) -> None:
     app = Client(session, api_id=API_ID, api_hash=API_HASH)
     is_authorized = await app.connect()
     if not is_authorized:
-        print(f'{session} failed')
+        print(f'{session} failed: not authorized.')
     try:
         me = await app.get_me()
     except (
@@ -25,16 +27,21 @@ async def check_client(session: str) -> None:
         errors.SessionRevoked,
         errors.UserDeactivated,
         errors.UserDeactivatedBan,
-    ):
-        print(f'{session} failed')
+    ) as exception:
+        print(f'{session} failed: {exception}.')
     else:
+        success_logged.append(session)
         print(f'"{session}" logged in as', me.phone_number)
 
 
 async def checker() -> tuple[int, int]:
     files = listdir('sessions')
-    print(files)
+    print(f'All sessions: {files}')
     sessions = [file_name[:-8] for file_name in files if file_name.endswith('.session')]
     await asyncio.gather(*[check_client(session) for session in sessions])
+    print()
+    print(f'{len(success_logged)} working sessions.')
+    for session in success_logged:
+        print(session)
 
 asyncio.run(checker())
